eb5d9ffc124898af311809460a8967bf03cd7121
kmp360 <52243093+kmp360@users.noreply.github.com>
2022-09-16 12:43:31 +0100

    Updated 2022-09-16

--- code/code-10-arrays.jl
+++ code/code-10-arrays.jl
@@ -1,5 +1,6 @@
-"Code example for Chapter 10 -- Arrays"
-
+# "Code example for Chapter 10 -- Arrays"
+#  C:\Users\kmpetersson\AppData\Local\Programs\Julia-1.8.0\bin\julia.exe
+#  cd("E:\\aaa-Julia-course-2022\\lectures-1.8")
 #---
 
 let
@@ -11,30 +12,35 @@ let
 end
 
 #---
-let
-    numbers = [42, 123]
 
-    for i in eachindex(numbers)
-        numbers[i] = numbers[i] * 2
-    end
 
-    numbers
+numbers = [42, 123]
+
+for i in eachindex(numbers)
+    numbers[i] = numbers[i] * 2
 end
 
+numbers
+
+
 #---
+
 t = ['a', 'b', 'c']
 push!(t, 'd')
 
 #---
+
 t1 = ['a', 'b', 'c']
 t2 = ['d', 'e']
 append!(t1, t2)
 
 #---
+
 t3 = ['d', 'c', 'e', 'b', 'a']
 sort!(t3)
 
 #---
+
 function addall(t)
     total = 0
     for x in t
@@ -44,6 +50,7 @@ function addall(t)
 end
 
 #---
+
 function capitalizeall(t)
     res = []
     for s in t
@@ -55,6 +62,7 @@ end
 capitalizeall("banana")
 
 #---
+
 function onlyupper(t)
     res = []
     for s in t
@@ -68,20 +76,24 @@ end
 onlyupper("bAnAnA")
 
 #---
+
 t = uppercase.(["abc", "def", "ghi"])
 print(t)
 
 #---
+
 t = ['a', 'b', 'c']
 splice!(t, 2)
 print(t)
 
 #---
+
 t = ['a', 'b', 'c']
 pop!(t)
 print(t)
 
 #---
+
 t = ['a', 'b', 'c']
 popfirst!(t)
 print(t)
@@ -93,6 +105,7 @@ print(deleteat!(t, 2))
 print(insert!(t, 2, 'x'))
 
 #---
+
 t = collect("spam")
 print(t)
 t = split("pining for the fjords")
@@ -106,6 +119,7 @@ s = join(t, ' ')
 print(s)
 
 #---
+
 a = [1, 2, 3]
 b = a
 b ≡ a
@@ -114,6 +128,7 @@ b[1] = 42
 print(a)
 
 #---
+
 t1 = [1, 2]
 t2 = push!(t1, 3);
 print(t1)
@@ -123,12 +138,15 @@ print(t1)
 print(t3)
 
 #---
+
+# errors!
 insert!(t, 4, x)
 push!(t, x)
 append!(t, [x])
 
 #---
-"Exercise 10-1"
+
+# "Exercise 10-1"
 
 function nestedsum(array)
     sum = 0
@@ -140,19 +158,20 @@ function nestedsum(array)
     return sum
 end
 
-nestedsum([4 5])
+nestedsum([4; 5])
 nestedsum([4 5; 6 7])
 nestedsum([[1 2], [3], [4 5; 6 7]])
 
 #---
-"Exercise 10-2"
+
+# "Exercise 10-2"
 
 function cumulsum(array)
     len = length(array)
     CS = zeros(len)
     sum = 0
     for k = 1:len
-        sum = sum + array[k]
+        sum += array[k]
         CS[k] = sum
     end
     return CS
@@ -162,7 +181,8 @@ cumulsum([4 5])
 cumulsum([4 5; 6 7])
 
 #---
-"Exercise 10-3"
+
+# "Exercise 10-3"
 
 function interior(array)
     a = array[:]
@@ -174,7 +194,8 @@ interior([6 7])
 interior([4 5 6 7])
 
 #---
-"Exercise 10-4"
+
+# "Exercise 10-4"
 
 function interior!(b)
     global b = b[2:end-1]
@@ -186,10 +207,11 @@ interior!(b)
 b
 
 #---
-"Exercise 10-5"
+
+# "Exercise 10-5"
 
 function issort(array)
-    a = sort(array, dims = 2)
+    a = sort(array,dims=2)
     return a == array
 end
 
@@ -198,11 +220,14 @@ issort([1 3 2])
 issort(['b' 'a'])
 issort(['a' 'b'])
 
+issort([2 3; 
+1 2])
 #---
-"Exercise 10-6"
+
+# "Exercise 10-6"
 
 function isanagram(word1::String, word2::String)::Bool
-    if sizeof(word1) != sizeof(word2)
+    if length(word1) != length(word2)
         return false
     else
         set1 = join(sort(collect(word1)))
@@ -211,10 +236,29 @@ function isanagram(word1::String, word2::String)::Bool
     end
 end
 
-isanagram("apa", "pap")
+function isanagram(word1::Int64, word2::String)::Bool
+    if length(word1) != length(word2)
+        return false
+    else
+        set1 = join(sort(collect(word1)))
+        set2 = join(sort(collect(word2)))
+        return set1 == set2
+    end
+end
+
+word1 = "abb🍌dcc"
+word2 = "cbcbda🍌"
+isanagram(word1, word2)
+
+set = union(word1, word2)
+
+isanagram('c', word2)
+
+length(123)
 
 #---
-"Exercise 10-7"
+
+# "Exercise 10-7"
 
 function hasduplicates(array)
     return !(sizeof(union(array)) == sizeof(array))
@@ -225,12 +269,13 @@ hasduplicates([1 2 1])
 hasduplicates([1 2 3])
 
 #---
-"Exercise 10-8"
+
+# "Exercise 10-8"
 
 function relfreq(n = 1e6)
     sum = 0
     for k = 1:n
-        if hasduplicates(rand(1:364, 23))
+        if hasduplicates(rand(1:365, 23))
             sum += 1
         end
     end
@@ -241,11 +286,11 @@ relfreq()
 relfreq(1000)
 
 #---
-"Exercise 10-9"
+
+# "Exercise 10-9"
 
 function createArray()
-
-    filename = "E:\\aaa-Julia-course-2022\\lectures\\words.txt"
+    filename = "E:\\aaa-Julia-course-2022\\lectures-1.8\\words.txt"
     fileIn = open(filename)
     lexicon = []
     for word in eachline(fileIn)
@@ -255,9 +300,9 @@ function createArray()
     return lexicon
 end
 
-
+# not an efficient way of solving the problem
 function createArray2()
-    filename = "E:\\aaa-Julia-course-2022\\lectures\\words.txt"
+    filename = "E:\\aaa-Julia-course-2022\\lectures-1.8\\words.txt"
     fileIn = open(filename)
     lexicon = []
     for word in eachline(fileIn)
@@ -269,20 +314,21 @@ function createArray2()
 end
 
 
-begin
-    lexicon = createArray()
-    lexicon2 = createArray2()
-end
 
+@time lexicon = createArray();
+
+@time lexicon2 = createArray2();  # takes about 2 min to run
 
 #---
-"Exercise 10-10"
+
+# "Exercise 10-10"
 
 function inbisect(array, word, flag = false)
     len = length(array) ÷ 2
     if len < 1
         return flag = false
     elseif len == 1
+        #@show (word in array)
         return flag = (word in array)
     elseif word > array[len]
         flag = inbisect(array[len+1:end], word, flag)
@@ -293,17 +339,19 @@ function inbisect(array, word, flag = false)
 end
 
 array = createArray()
+inbisect(array, "aardvark")
 
-inbisect(array, "aadvark")
+#@time inbisect(array, "aardvark")
 
 #---
-"Exercise 10-11"
+
+# "Exercise 10-11"
 
 function reversepairs()
     array = createArray()
     pairarray = []
     for word in array
-        reverseword = word[end:-1:1]
+        reverseword = reverse(word)
         if inbisect(array, reverseword)
             push!(pairarray, [word, reverseword])
         end
@@ -313,8 +361,11 @@ end
 
 pairs = reversepairs()
 
+@time pairs = reversepairs();
+
 #---
-"Exercise 10-12"
+
+# "Exercise 10-12"
 
 function interlock()
 
@@ -338,13 +389,16 @@ function interlock()
     return intrlock_1, intrlock_2
 end
 
+@time intrloc_1, intrloc_2 = interlock();
 
+intrloc_1
+
+intrloc_2
+
+#==
 using BenchmarkTools
 
-@btime begin
-
-    intrloc_1, intrloc_2 = interlock()
-
-end
+@btime  intrloc_1, intrloc_2 = interlock();
+==#
 
 #=============================================================================#
